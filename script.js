f5922cc8c982716136c6d9826e31d232a99b7ee7
Jsieler <joshuasieler@Joshs-MacBook-Air.local>
2022-03-27 21:07:44 -0600

    setting up page layout, and event listeners

--- script.js
+++ script.js
@@ -10,20 +10,9 @@ const tequila = ['Margarita', 'Paloma', 'Tequila Sunrise']
 const whiskey = ['Sazerac', 'Whiskey Sour', 'Hot Toddy', 'Manhattan', 'Old Fashioned', 'Bourbon Peach Punch']
 const rum = ['Daiquiri', 'Mai Tai', 'Pina Colada', 'Dark N Stormy', 'Jungle Bird', 'Coquito', 'Painkiller', 'Mojito']
 
-// Vodka Button
-vodkaBtnEl.addEventListener('click', function () {
-    displayDrinkEl.textContent = vodka[Math.floor(Math.random() * vodka.length)]
-})
-// Tequila Button
-tequilaBtnEl.addEventListener('click', function () {
-    displayDrinkEl.textContent = tequila[Math.floor(Math.random() * tequila.length)]
-})
-// Whiskey Button
-whiskeyBtnEl.addEventListener('click', function () {
-    displayDrinkEl.textContent = whiskey[Math.floor(Math.random() * whiskey.length)]
-})
-// Rum Button
-rumBtnEl.addEventListener('click', function () {
-    displayDrinkEl.textContent = rum[Math.floor(Math.random() * rum.length)]
-})
+function getRandomDrink(liqour) {
+    displayDrinkEl.textContent = liqour[Math.floor(Math.random() * liqour.length)]
+}
+
+
 
