873b91004da38e5723c5956d9726d0357d7e500e
Jsieler <joshuasieler@Joshs-MacBook-Air.local>
2022-03-29 06:41:37 -0600

    added functionality to generate and display random liqour and cocktails in the HTML

--- script.js
+++ script.js
@@ -3,16 +3,64 @@ let tequilaBtnEl = document.getElementById('tequila-button')
 let whiskeyBtnEl = document.getElementById('whiskey-button')
 let rumBtnEl = document.getElementById('rum-button')
 let displayDrinkEl = document.getElementById('display-drink-container')
+let displayCocktailEl = document.getElementById('display-cocktail-container')
 
 
-const vodka = ['Bloody Mary', 'Screw Driver', 'Cosmopolitan', 'Espresso Martini', 'Moscow Mule', 'White Russian']
-const tequila = ['Margarita', 'Paloma', 'Tequila Sunrise']
-const whiskey = ['Sazerac', 'Whiskey Sour', 'Hot Toddy', 'Manhattan', 'Old Fashioned', 'Bourbon Peach Punch']
-const rum = ['Daiquiri', 'Mai Tai', 'Pina Colada', 'Dark N Stormy', 'Jungle Bird', 'Coquito', 'Painkiller', 'Mojito']
+// Liqour Types and Cocktails 
+const vodka = {
+    liqourName: 'Vodka',
+    cocktails: ['Bloody Mary', 'Screw Driver', 'Cosmopolitan', 'Espresso Martini', 'Moscow Mule', 'White Russian'],
+  };
+const tequila = {
+    liqourName: 'Tequila',
+    cocktails: ['Margarita', 'Paloma', 'Tequila Sunrise']
+};
+const whiskey = {
+    liqourName: 'Whiskey',
+    cocktails: ['Sazerac', 'Whiskey Sour', 'Hot Toddy', 'Manhattan', 'Old Fashioned', 'Bourbon Peach Punch']
+};
+const rum = {
+    liqourName: 'Rum',
+    cocktails: ['Daiquiri', 'Mai Tai', 'Pina Colada', 'Dark N Stormy', 'Jungle Bird', 'Coquito', 'Painkiller', 'Mojito']
+};
+const liqourDrinks = [vodka, tequila, whiskey, rum]
 
-function getRandomDrink(liqour) {
-    displayDrinkEl.textContent = liqour[Math.floor(Math.random() * liqour.length)]
+
+// Functions to get Random Drinks onClick  
+
+function getRandomDrink() {
+    // Gets Random Liqour
+    randomLiqour = Math.floor(Math.random()*liqourDrinks.length)
+    displayDrinkEl.textContent = liqourDrinks[`${randomLiqour}`].liqourName
+    // Gets Random Cocktail from Liqour 
+    randomDrink = Math.floor(Math.random()*liqourDrinks[`${randomLiqour}`].cocktails.length)
+    displayCocktailEl.textContent = liqourDrinks[`${randomLiqour}`].cocktails[`${randomDrink}`]
 }
 
+function getVodkaDrink() {
+    randomDrink = Math.floor(Math.random()*liqourDrinks[0].cocktails.length)
+    console.log(randomDrink)
+    displayDrinkEl.textContent = 'Vodka'
+    displayCocktailEl.textContent = liqourDrinks[0].cocktails[`${randomDrink}`]
+}
 
+function getTequilaDrink() {
+    randomDrink = Math.floor(Math.random()*liqourDrinks[1].cocktails.length)
+    console.log(randomDrink)
+    displayDrinkEl.textContent = 'Tequila'
+    displayCocktailEl.textContent = liqourDrinks[1].cocktails[`${randomDrink}`]
+}
 
+function getWhiskeyDrink() {
+    randomDrink = Math.floor(Math.random()*liqourDrinks[2].cocktails.length)
+    console.log(randomDrink)
+    displayDrinkEl.textContent = 'Whiskey'
+    displayCocktailEl.textContent = liqourDrinks[2].cocktails[`${randomDrink}`]
+}
+
+function getRumDrink() {
+    randomDrink = Math.floor(Math.random()*liqourDrinks[3].cocktails.length)
+    console.log(randomDrink)
+    displayDrinkEl.textContent = 'Rum'
+    displayCocktailEl.textContent = liqourDrinks[3].cocktails[`${randomDrink}`]
+}
