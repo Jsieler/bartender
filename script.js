63e888a374d644f02c9b210976c1fd6ac45ea2be
Jsieler <joshuasieler@Joshs-MacBook-Air.local>
2022-04-01 21:16:05 -0600

    added more info for ingredients

--- script.js
+++ script.js
@@ -6,36 +6,97 @@ let displayDrinkEl = document.getElementById('display-drink-container')
 let displayCocktailEl = document.getElementById('display-cocktail-container')
 let displayDescriptionEl = document.getElementById('display-description-container')
 let displayImageEl = document.getElementById('display-image-container')
+let displayIngredientsListEl = document.getElementById('ingredients-list')
 
 // Liqour Types and Cocktails 
 const vodka = {
     liqourName: 'Vodka',
     cocktails: ['Bloody Mary', 'Screw Driver', 'Cosmopolitan', 'Espresso Martini', 'Moscow Mule', 'White Russian'],
-    description: ['combine vodka, tomato juice, lemon juice, Worcestershire sauce, pepper, and celery salt in a Highball glass. Top with your garnish of choice','made by combining vodka and orange juice in a Highball glass with ice', 'made with vodka, cranberry juice, lime, and Triple Sec', 'combination of vodka, coffee liqueur, fresh espresso, and simple syrup. Shake all ingredients together, strain into a Martini or coupe glass', 'add vodka, ginger beer, and lime juice to a copper mug with crushed ice, and garnish with a lime wedge.', 'White Russian is a simple combination of half and half (or milk), vodka, and coffee liqueur.'],
-    image: ['BloodyMary.webp','ScrewDriver.webp','Cosmopolitan.webp','EspressoMartini.webp','MoscowMule.webp','WhiteRussian.webp']
-  };
+    description: [
+    'Combine all ingredients in a Collins or highball glass. Add ice. Stir well. Garnish with lemon, celery, olives, and enjoy.ombine vodka, tomato juice, lemon juice, Worcestershire sauce, pepper, and celery salt in a Highball glass. Top with your garnish of choice',
+    'Made by combining vodka and orange juice in a Highball glass with ice',
+    'Combine all ingredients in shaker tinAdd ice. Shake vigorously, until tin is frosted over.Strain into chilled cocktail glass Garnish with lime twist and enjoy.',
+    'Combine all ingredients in a mixing tin and shake vigorously with ice. Strain into a coupette glass. Garnish with three coffee beans.',
+    'Add vodka, ginger beer, and lime juice to a copper mug (or highball glass). Fill mug with crushed ice. Stir well. Garnish with lime wedge and enjoy.',
+    'Combine all ingredients in mixing glass with ice. Stir Strain into chilled rocks glass over fresh ice'],
+
+    image: ['BloodyMary.webp','ScrewDriver.webp','Cosmopolitan.webp','EspressoMartini.webp','MoscowMule.webp','WhiteRussian.webp'],
+    ingredients: [
+    '2 oz Vodka 5 oz Tomato juice 1/2 oz Fresh lemon Juice ½ oz Worcestershire Pinch PepperPinch Celery salt',
+    '2 oz Vodka As needed Orange juice',
+    '1 1/2 oz Vodka ½ oz Cranberry juice ¾ oz Lime juice ¾ oz Cointreau L unique',
+    '1 ounce vodka, preferably Belvedere Smógory Forest ¾ ounce coffee liqueur, preferably Mr. Black Cold Brew Coffee Liqueur1 ¼ ounces fresh espresso½ ounce simple syrup, (1:1, sugar:water) Garnish: 3 coffee beans',
+    '2 ounces vodka 3 ounces ginger beer Juice of half a lime, and lime wedge for garnish',
+    '1 ¼ oz Vodka1 ¼ oz Half and half1 ¼ oz Coffee liqueur (Kahlua)']
+};
 const tequila = {
     liqourName: 'Tequila',
     cocktails: ['Margarita', 'Paloma', 'Tequila Sunrise', 'After Hours Bloody Maria', 'Ranch Water'],
-    description: ['Just combine blanco tequila with lime juice and the orange liqueur of your choice, shake with ice, and strain into a glass with ice. Garnish with a lime', 'add reposado or blanco tequila to your glass, and top with lime juice, salt, and grapefruit soda', 'add tequila and orange juice to a Highball glass, and use a spoon to sink grenadine to the bottom of the glass', 'While the classic recipe simply combines tequila with tomato juice, lemon juice, Worcestershire sauce, and pepper, this version adds chocolate-chili bitters', 'A simple combination of Topo Chico, blanco tequila, and lime juice'],
-    image: ['Margarita.jpeg','Paloma.jpeg','TequilaSunrise.jpeg','AfterHoursBloodyMaria.jpeg','RanchWater.webp']
+    description: [
+    'Combine all ingredients in shaker tin. Add ice to small shaker tin. Shake vigorously. Strain into chilled rocks glass with a salt rim over fresh ice Garnish with lime and enjoy.',
+     'Combine ingredients in a highball. Add ice. Garnish with a grapefruit wheel and enjoy.',
+     'Combine ingredients in a highball, excluding grenadine Add ice. Using a spoon, sink grenadine to the bottom of the glass. Garnish with an orange wheel and enjoy.',
+     'Rim half of a chilled Collins glass in agave nectar, followed by salt. Put all other ingredients into a cocktail shaker with ice, and shake vigorously for 10 seconds. Pour into your rimmed glass over four ice cubes, and stir with a bar spoon.',
+     'Start by pouring out (or drinking) two ounces of the Topo Chico. Add the tequila then lime juice to the Topo Chico bottle.'],
+    image: ['Margarita.jpeg','Paloma.jpeg','TequilaSunrise.jpeg','AfterHoursBloodyMaria.jpeg','RanchWater.webp'],
+    ingredients: [
+        '1 ¾ oz Blanco tequila 1 oz Lime juice ¾ oz Triple Sec',
+         '2 oz Reposado or Blanco tequila 1 oz Lime 1 each Pinch of salt As needed Grapefruit soda',
+         '2 oz Tequila As needed Orange juice 1/2 oz Grenadine',
+         '6 ounces Bloody Mary mix, such as Toma 2 ounces reposado tequila, such as Goza ½ ounce lime juice 6 dashes of chocolate-chili bitters Agave nectar and flake sea salt or sal de gasano, for rimming glass (optional)',
+         '12-ounce bottle Topo Chico, chilled 1 ½ ounces blanco tequila ½ ounce fresh lime juice'
+    ]
 };
 const whiskey = {
     liqourName: 'Whiskey',
     cocktails: ['Sazerac', 'Whiskey Sour', 'Manhattan', 'Old Fashioned', 'Bourbon Peach Punch'],
-    description: ['Just add some rye, Demerara syrup, Peychaud’s bitters, and your desired amount of absinthe into a mixing glass and stir. Garnish with a lemon rind and toast to NOLA', 'whiskey (typically bourbon), simple syrup, and any kind of citrus juice.', 'made with bourbon or rye, though some more modern versions opt for bourbon. Just mix together the whiskey of your choice, sweet vermouth, some bitters, and garnish with a Maraschino cherry or an orange peel', 'healthy helping of bourbon or rye, a bit of simple syrup (or a sugar cube), and some Angostura bitters', 'This recipe uses frozen peach slices in place of ice cubes, so there’s no need to worry about a diluted drink if you’re imbibing during the afternoon of a summer scorcher. It also calls for pineapple and lemon juice, and a dash of black pepper'],
-    image: ['Sazerac.webp','WhiskeySour.webp','Manhattan.webp','OldFashioned.webp','BourbonPeachPunch.webp']
+    description: [
+    'Combine all ingredients in mixing glass with ice. Stir. Strain into chilled rocks glass rinsed with absinthe, with no ice. Garnish with lemon twist and enjoy',
+    'Add all ingredients to a mixing tin and shake with ice (wet shake) until properly chilled. Remove the ice and shake again (dry shake) to emulsify. Double strain over ice into a rocks glass and garnish as you please.',
+    'Combine all ingredients in mixing glass with ice. Stir. Strain into chilled cocktail glass Garnish with a cherry and enjoy.',
+    'Combine first three ingredients in a rocks glass. Stir ingredients. Add ice. Garnish with a lemon and orange twist and enjoy.',
+    'Make peach ice cubes by adding 1 sliced peach into each cavity of an ice cube tray that makes 6 large cubes. Fill with water and place in freezer. After ice cubes are hard, muddle two peach slices in the bottom of a shaker. Add bourbon, lemon juice and ice and shake to mix and chill. Place one peach ice cube in a tumbler glass and strain contents of shaker over it. Top with an ounce of pineapple juice and fresh-cracked black pepper.'],
+    image: ['Sazerac.webp','WhiskeySour.webp','Manhattan.webp','OldFashioned.webp','BourbonPeachPunch.webp'],
+    ingredients: [
+        '3 oz Rye or Brandy 1/2 oz Demerara syrup 10 dashes Peychauds Bitters 1 ea Lemon twistAs needed Absinthe',
+        '2 ounces whiskey (preferably bourbon) 1 ounce lemon juice ¾ ounce simple syrup 1 egg white Garnish: brandied cherry, Angostura bitters (to taste), or lemon peel',
+        '2 ½ oz Rye 1 oz Sweet Vermouth 2 dashes Angostura',
+        '2 ½ oz Bourbon (or Rye Whiskey) 1/2 oz simple syrup 3 dash Angostura Bitters 1 each Lemon and orange twist',
+        '1.5 ounces bourbon 1 ounce pineapple juice 1 ounce lemon juice 1 peach sliced into 8 slices black pepper to garnish'
+    ]
 };
 const rum = {
     liqourName: 'Rum',
     cocktails: ['Daiquiri', 'Mai Tai', 'Pina Colada', 'Dark N Stormy', 'Jungle Bird', 'Coquito', 'Painkiller', 'Mojito'],
-    description: ['combine white rum, lime juice, and simple syrup in a shaker, and strain over ice. Garnish with a lime wedge', 'It’s made with blended rum, lime juice, orgeat syrup, and orange liqueur, shaken together with ice. Serve it in a double rocks glass complete with crushed ice and a mint sprig.', 'combination of white rum, coconut cream, and pineapple juice. It’s often served frozen, but it’s also tasty shaken and served on the rocks in a Hurricane glass. Garnish with pineapple leaves and a slice of pineapple.', ' Simply add ginger beer to a Highball glass with ice, and float Goslings rum on top. Garnish with a lime wedge', 'The classic Tiki drink is made with rum, Campari, lime juice, simple syrup, and pineapple juice. It’s best served in a Highball glass with ice and a pineapple wedge garnish', 'silver rum with coconut milk, cream of coconut, evaporated milk, and sweetened condensed milk. To make it, blend all ingredients together, then shake with a tequila wash. Strain into a coupe glass, and top with a sprinkle of nutmeg.', 'the Painkiller combines rum, pineapple juice, orange juice, and cream of coconut. Serve in the cocktail glass of your choice, and garnish with a sprinkle of nutmeg, pineapple leaves, and an orange wedge', 'muddle about 10 mint leaves in a glass, then top with white rum, lime juice, and simple syrup. Add lots of crushed ice, and garnish with a mint sprig for good measure.'],
-    image: ['Daiquiri.webp','MaiTai.webp','PinaColada.webp','DarkNStormy.webp','JungleBird.webp','Coquito.webp','PainKiller.webp','Mojito.webp']
+    description: [
+    'Combine all ingredients, except lime wedge, in a shaker. Add ice and shake vigorously, until tin is frosted over. Strain cocktail into a chilled coupe glass. Garnish with a lime wedge and enjoy.',
+     'Combine all ingredients, except mint, in a shaker. Add crushed ice and shake vigorously, until shaker is frosted over. Fill a double rocks glass with fresh crushed ice. Strain cocktail into glass. Garnish with mint sprig.',
+     'Combine all ingredients, except pineapple leaf, in shaker. Add ice to fill. Shake vigorously, until shaker is frosted over. Fill hurricane glass with crushed ice. Strain cocktail into glass. Garnish with pineapple leaf and enjoy.',
+    'Pour Ginger beer in to highball glass over ice. Add ice. Float Goslings Black Seal Rum on top. Garnish with lime wedge and enjoy.',
+     'Combine all ingredients in a cocktail shaker with ice, and shake to combine and chill. Pour into a rocks glass over fresh ice.',
+    'Combine all ingredients except the tequila in a mixing bowl or blender, and blend on high for 2 minutes. Bottle the mixture and set in the refrigerator for at least 2 hours to settle. To serve, pour 3-4 ounces of chilled coquito into a shaker with an ounce of rum or añejo tequila and shake without ice. Pour into glass and garnish with a sprinkle of cinnamon.',
+    'Combine ingredients in a cocktail shaker with plenty of crushed ice. Shake well, and then pour entire contents of the shaker into your glassware of choice. Top with generous sprinkle of nutmeg, and garnish with pineapple fronds and an orange wedge.',
+    'Gently muddle mint leaves in Collins or highball glass, taking care not to break the leaves. Add remaining ingredients to the glass. Mix well. Add crushed ice. Garnish with mint sprig and enjoy.'],
+    image: ['Daiquiri.webp','MaiTai.webp','PinaColada.webp','DarkNStormy.webp','JungleBird.webp','Coquito.webp','PainKiller.webp','Mojito.webp'],
+    ingredients: [
+        '2 oz white rum ¾ oz lime juice ¾ oz simple syrup Lime wedge',
+        '2 ounces blended rum, such as Appleton Estate Reserve Blend or Denizen Merchant’s Reserve ¾ ounce lime juice ¾ ounce orgeat syrup ½ ounce orange liqueur Mint sprig',
+        '2 oz White Rum 2 oz Coconut Cream 2 oz Pineapple Juice Pineapple leaf',
+        '2 oz Goslings Black Seal Rum As needed Ginger beer',
+        '1 ½ ounces rum, Jamaican or blackstrap ¾ ounce Campari ½ ounce fresh lime juice ½ ounce simple syrup 1 ½ ounces pineapple juice',
+        '12 ounces silver rum 15 ounces cream of coconut 12 ounces coconut milk 12 ounces sweetened condensed milk 12 ounces evaporated milk ¾ teaspoon ground nutmeg añejo tequila or silver rum wash',
+        '2 to 4 ounces of Pusser’s Navy Rum 4 ounces unsweetened pineapple juice 1 ounce freshly squeezed orange juice 1 ounce cream of coconut',
+        '2 oz White Rum 1 oz Lime 3/4 oz Simple Syrup 10 Mint leaves, and mint sprig for garnish'
+    ]
 };
 const liqourDrinks = [vodka, tequila, whiskey, rum]
 
-// Functions to get Random Drinks onClick  
 
+function showIngredients() {
+    displayIngredientsListEl.style.color = 'white'
+}
+
+// Functions to get Random Drinks onClick  
 function getRandomDrink() {
     // Gets Random Liqour
     randomLiqour = Math.floor(Math.random()*liqourDrinks.length)
@@ -44,11 +105,13 @@ function getRandomDrink() {
     randomDrink = Math.floor(Math.random()*liqourDrinks[`${randomLiqour}`].cocktails.length)
     displayCocktailEl.textContent = liqourDrinks[`${randomLiqour}`].cocktails[`${randomDrink}`]
     displayDescriptionEl.textContent = liqourDrinks[`${randomLiqour}`].description[`${randomDrink}`]
+    displayIngredientsListEl.textContent = liqourDrinks[`${randomLiqour}`].ingredients[`${randomDrink}`]
     drinkImage = liqourDrinks[`${randomLiqour}`].image[`${randomDrink}`]
     image = new Image
     image.src = `./images/${drinkImage}`
     displayImageEl.innerHTML = ""
     displayImageEl.append(image)
+    showIngredients()
 }
 
 function getVodkaDrink() {
@@ -56,11 +119,13 @@ function getVodkaDrink() {
     displayDrinkEl.textContent = 'Vodka'
     displayCocktailEl.textContent = liqourDrinks[0].cocktails[`${randomDrink}`]
     displayDescriptionEl.textContent = liqourDrinks[0].description[`${randomDrink}`]
+    displayIngredientsListEl.textContent = liqourDrinks[0].ingredients[`${randomDrink}`]
     drinkImage = liqourDrinks[0].image[`${randomDrink}`]
     image = new Image
     image.src = `./images/${drinkImage}`
     displayImageEl.innerHTML = ""
     displayImageEl.append(image)
+    showIngredients()
 }
 
 function getTequilaDrink() {
@@ -68,11 +133,13 @@ function getTequilaDrink() {
     displayDrinkEl.textContent = 'Tequila'
     displayCocktailEl.textContent = liqourDrinks[1].cocktails[`${randomDrink}`]
     displayDescriptionEl.textContent = liqourDrinks[1].description[`${randomDrink}`]
+    displayIngredientsListEl.textContent = liqourDrinks[1].ingredients[`${randomDrink}`]
     drinkImage = liqourDrinks[1].image[`${randomDrink}`]
     image = new Image
     image.src = `./images/${drinkImage}`
     displayImageEl.innerHTML = ""
     displayImageEl.append(image)
+    showIngredients()
 }
 
 function getWhiskeyDrink() {
@@ -80,11 +147,13 @@ function getWhiskeyDrink() {
     displayDrinkEl.textContent = 'Whiskey'
     displayCocktailEl.textContent = liqourDrinks[2].cocktails[`${randomDrink}`]
     displayDescriptionEl.textContent = liqourDrinks[2].description[`${randomDrink}`]
+    displayIngredientsListEl.textContent = liqourDrinks[2].ingredients[`${randomDrink}`]
     drinkImage = liqourDrinks[2].image[`${randomDrink}`]
     image = new Image
     image.src = `./images/${drinkImage}`
     displayImageEl.innerHTML = ""
     displayImageEl.append(image)
+    showIngredients()
 }
 
 function getRumDrink() {
@@ -92,10 +161,12 @@ function getRumDrink() {
     displayDrinkEl.textContent = 'Rum'
     displayCocktailEl.textContent = liqourDrinks[3].cocktails[`${randomDrink}`]
     displayDescriptionEl.textContent = liqourDrinks[3].description[`${randomDrink}`]
+    displayIngredientsListEl.textContent = liqourDrinks[3].ingredients[`${randomDrink}`]
     drinkImage = liqourDrinks[3].image[`${randomDrink}`]
     image = new Image
     image.src = `./images/${drinkImage}`
     displayImageEl.innerHTML = ""
     displayImageEl.append(image)
+    showIngredients()
 }
 
